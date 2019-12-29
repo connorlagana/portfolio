b3d40897c944bee54ae219e5179a257d62eeab83
Connor Lagana <connor.lagana@gmail.com>
2019-12-29 14:24:41 -0500

    background image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import NavBar from "./NavBar.js"
+import codeBackground from '../images/codeBackground.jpeg'
 
 const Header = () => {
   return (
     <div>
+      <img src={codeBackground} />
       <h1>Connor Lagana</h1>
       <h3>Software Engineer</h3>
       <NavBar />
