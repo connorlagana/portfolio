b91c154e4128c46bc56ade0fb1242afcb101cf41
Connor Lagana <connor.lagana@gmail.com>
2019-12-30 22:20:43 -0500

    facts

--- src/components/About.js
+++ src/components/About.js
@@ -10,9 +10,17 @@ const About = () => {
         <img src={wheretheladies} id='sexyAssConnor' />
         <div id="blueFrame">
           <h1>Quick Facts:</h1>
-          <p>Legal Name: Connor Lagana</p>
-          <p>Preferred Name: Titanium Dragon</p>
-          <p>Preferred Name: Titanium Dragon</p>
+          <p>Legal name: Connor Lagana</p>
+          <p>Preferred name: Titanium Dragon</p>
+          <p>Social Security Number: 834-71-2909</p>
+          <p>FICO Score: 745</p>
+          <p>Country: US of A</p>
+          <p>State: Depressed</p>
+          <p>Favorite amendment: Twenty-first</p>
+          <p>Dunky order: Medium Iced Coffee w/ Almond Milk</p>
+          <p>Amount of brown rice consumed daily: 1.3 lbs</p>
+          <p>Relationship status: Single and ready to mingle</p>
+          <p>Highest number counted to: 1,602</p>
         </div>
       </div>
     </div>
