cdcad1ee314f1bd9cd23045b9f16c006e97f8b73
Connor Lagana <connor.lagana@gmail.com>
2019-12-30 20:43:30 -0500

    added technologies

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import sexyAssConnor from '../images/sexyAssConnor.jpg'
+import wheretheladies from '../images/wheretheladies.PNG'
 
 const About = () => {
   return (
     <div>
       <h1 className="sectionTitle">About</h1>
       <div id='profile'>
-        <img src={sexyAssConnor} id='sexyAssConnor' />
-        <div id="blueFrame">I am a full stack developer with a passion for beautiful
-        and powerful products. My editorial background gives me an astute eye for details
-        and I view everything through an empathic lens. Most importantly, I’m always
-        learning. Interested in working together? Shoot me an email.</div>
+        <img src={wheretheladies} id='sexyAssConnor' />
+        <div id="blueFrame">What up!!! We're three cool guys looking for other cool
+        guys who wanna hang out in our party mansion. Nothing sexual. Dudes in good
+        shape encouraged, if you're fat you should be able to find humor in the
+        little things. Again, <u>NOTHING SEXUAL.</u></div>
       </div>
     </div>
   )
